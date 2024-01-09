b76064087ddcce4d9e2e414e99a1efbec08efce4
Amar Singh <gujraal2006@gmail.com>
2024-01-09 19:34:06 +0530

    first commit

---
+++ controllers/viewController.js
@@ -0,0 +1,18 @@
+exports.home = async (req, res, next) => {
+  res.status(200).render("index");
+};
+
+exports.contact = async (req, res, next) => {
+  res.status(200).render("contact");
+};
+
+exports.about = async (req, res, next) => {
+  res.status(200).render("about");
+};
+
+exports.shop = async (req, res, next) => {
+  res.status(200).render("shop");
+};
+exports.login = async (req, res, next) => {
+  res.status(200).render('login');
+};
---
+++ routes/viewRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const viewController = require('../controllers/viewController')
+const router = express.Router({ mergeParams: true });
+
+router.get("/", viewController.home);
+router.get("/about", viewController.about);
+router.get("/contact", viewController.contact);
+router.get("/shop", viewController.shop);
+router.get("/login", viewController.login);
+
+
+
+module.exports = router;
